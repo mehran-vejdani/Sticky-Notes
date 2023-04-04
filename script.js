1590a9fe6297c035eca206e312e2509e15b8bb0a
mehran vejdani <mehran.vejdanii@gmail.com>
2023-04-04 22:16:58 +0430

    get selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+let container2 = document.querySelector(".container2");
+let container3 = document.querySelector(".container3");
+let checkIcon = document.querySelector(".check-icon");
+let XIcon = document.querySelector(".Xcheck-icon");
